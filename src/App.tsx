4ef470ca5723839d77637a89dd2edffcd3dcd1b1
Carter Dennis <carter@landscout.com>
2022-06-03 23:29:45 -0500

    Display a map centered on U.S.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef } from 'react';
 import './App.css';
 import esriConfig from "@arcgis/core/config";
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
 
-export default function App() {
-  esriConfig.apiKey = process.env.ESRI_API_KEY as string;
-  console.log(process.env.ESRI_API_KEY as string)
+esriConfig.apiKey = process.env.REACT_APP_ESRI_API_KEY as string;
+
+export default function App(props: any) {
+  const mapDiv = useRef(null);
+
+  React.useEffect(() => {
+    if (mapDiv.current) {
+      const map = new Map({
+        basemap: "topo-vector"
+      });
+
+      const view = new MapView({
+        map: map,
+        center: [-97.30, 35.28],
+        zoom: 5,
+        constraints: {
+          rotationEnabled: false
+        },
+        container: mapDiv.current
+      });
+    }
+  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="mapDiv" ref={mapDiv} style={{ height: "100vh", width: "70vw" }}>
+
     </div>
   );
 }
