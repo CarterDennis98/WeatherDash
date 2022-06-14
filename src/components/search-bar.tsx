df13de64c79730023269d2c1c2a15fc6112c6ad3
Carter Dennis <carter.j.dennis@gmail.com>
2022-06-14 16:55:20 -0500

    Added Google Places to Autocomplete in search bar

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,8 +1,13 @@
-import SearchIcon from "@mui/icons-material/Search";
-import IconButton from "@mui/material/IconButton";
-import InputAdornment from "@mui/material/InputAdornment";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Autocomplete from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
-import { makeStyles } from "@mui/styles";
+import Typography from "@mui/material/Typography";
+import { makeStyles } from '@mui/styles';
+import parse from "autosuggest-highlight/parse";
+import throttle from "lodash/throttle";
+import * as React from "react";
 
 const searchBarStyle = makeStyles(() => ({
     root: {
@@ -18,29 +23,168 @@ const searchBarStyle = makeStyles(() => ({
     }
 }));
 
+function loadScript(src: string, position: HTMLElement | null, id: string) {
+    if (!position) {
+        return;
+    }
+
+    const script = document.createElement("script");
+    script.setAttribute("async", "");
+    script.setAttribute("id", id);
+    script.src = src;
+    position.appendChild(script);
+}
+
+const autocompleteService = { current: null };
+
+interface MainTextMatchedSubstrings {
+    offset: number;
+    length: number;
+}
+interface StructuredFormatting {
+    main_text: string;
+    secondary_text: string;
+    main_text_matched_substrings: readonly MainTextMatchedSubstrings[];
+}
+interface PlaceType {
+    description: string;
+    structured_formatting: StructuredFormatting;
+}
+
 export default function SearchBar(props: any) {
     const searchBarClasses = searchBarStyle();
 
-    const onChange = (event: any) => {
+    const [value, setValue] = React.useState<PlaceType | null>(null);
+    const [inputValue, setInputValue] = React.useState("");
+    const [options, setOptions] = React.useState<readonly PlaceType[]>([]);
+    const loaded = React.useRef(false);
 
+    if (typeof window !== "undefined" && !loaded.current) {
+        if (!document.querySelector("#google-maps")) {
+            loadScript(
+                `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&libraries=places`,
+                document.querySelector("head"),
+                "google-maps",
+            );
+        }
+
+        loaded.current = true;
     }
 
+    // Get place predictions from Google Places API
+    const fetch = React.useMemo(
+        () =>
+            throttle(
+                (
+                    request: { input: string },
+                    callback: (results?: readonly PlaceType[]) => void,
+                ) => {
+                    (autocompleteService.current as any).getPlacePredictions(
+                        request,
+                        callback,
+                    );
+                },
+                200,
+            ),
+        [],
+    );
+
+    React.useEffect(() => {
+        let active = true;
+
+        if (!autocompleteService.current && (window as any).google) {
+            autocompleteService.current = new (
+                window as any
+            ).google.maps.places.AutocompleteService();
+        }
+        if (!autocompleteService.current) {
+            return undefined;
+        }
+
+        if (inputValue === "") {
+            setOptions(value ? [value] : []);
+            return undefined;
+        }
+
+        fetch({ input: inputValue }, (results?: readonly PlaceType[]) => {
+            if (active) {
+                let newOptions: readonly PlaceType[] = [];
+
+                if (value) {
+                    newOptions = [value];
+                }
+
+                if (results) {
+                    newOptions = [...newOptions, ...results];
+                }
+
+                setOptions(newOptions);
+            }
+        });
+
+        return () => {
+            active = false;
+        };
+    }, [value, inputValue, fetch]);
+
     return (
-        <TextField
-            onChange={onChange}
-            variant="outlined"
-            placeholder="Search"
+        <Autocomplete
             size="small"
-            InputProps={{
-                endAdornment: <InputAdornment position="end">
-                    {
-                        <IconButton onClick={() => console.log("search")} sx={{ color: "white" }}>
-                            <SearchIcon />
-                        </IconButton>
-                    }
-                </InputAdornment>
+            sx={{width: "500px"}}
+            getOptionLabel={(option) =>
+                typeof option === "string" ? option : option.description
+            }
+            filterOptions={(x) => x}
+            options={options}
+            autoComplete
+            includeInputInList
+            filterSelectedOptions
+            value={value}
+            onChange={(event: any, newValue: PlaceType | null) => {
+                setOptions(newValue ? [newValue, ...options] : options);
+                setValue(newValue);
             }}
-            className={searchBarClasses.root}
+            onInputChange={(event, newInputValue) => {
+                setInputValue(newInputValue);
+            }}
+            renderInput={(params) => (
+                <TextField className={searchBarClasses.root} {...params} fullWidth placeholder="Search Locations" />
+            )}
+            renderOption={(props, option) => {
+                const matches = option.structured_formatting.main_text_matched_substrings;
+                const parts = parse(
+                    option.structured_formatting.main_text,
+                    matches.map((match: any) => [match.offset, match.offset + match.length]),
+                );
+
+                return (
+                    <li {...props}>
+                        <Grid container alignItems="center">
+                            <Grid item>
+                                <Box
+                                    component={LocationOnIcon}
+                                    sx={{ color: "text.secondary", mr: 2 }}
+                                />
+                            </Grid>
+                            <Grid item xs>
+                                {parts.map((part, index) => (
+                                    <span
+                                        key={index}
+                                        style={{
+                                            fontWeight: part.highlight ? 700 : 400,
+                                        }}
+                                    >
+                                        {part.text}
+                                    </span>
+                                ))}
+                                <Typography variant="body2" color="text.secondary">
+                                    {option.structured_formatting.secondary_text}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </li>
+                );
+            }}
         />
-    )
+    );
 }
