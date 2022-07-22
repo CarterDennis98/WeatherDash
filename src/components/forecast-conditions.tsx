72f600c176be6841ab0ff969703a6c9158f1e0fc
Carter Dennis <carter.j.dennis@gmail.com>
2022-07-22 09:46:26 -0500

    Updated styling for forecast bar some more

--- src/components/forecast-conditions.tsx
+++ src/components/forecast-conditions.tsx
@@ -1,24 +1,20 @@
 import Tooltip from '@mui/material/Tooltip';
-/*
-<p style={{ margin: 0, color: "white", fontSize: "small", display: "flex", justifyContent: "center" }}>
-                        <b>{forecast ? forecast.name : ""}</b>
-                    </p>
-                    */
+
 export default function ForecastConditions(props: any) {
     let forecast = props.forecast ? props.forecast.properties.periods[props.period] : null;
 
     return props.forecast ? (
         <div style={{ display: "flex", justifyContent: "flex-start", minWidth: "250px", alignItems: "center" }}>
             <div style={{ display: "flex", flexDirection: "row", alignItems: "center", height: "100%" }}>
-                <div style={{ height: "100%", display: "flex", alignItems: "center", position: "relative" }}>
-                    <div style={{ height: "15px", width: "100%", position: "absolute", backgroundColor: "#808080ad", top: "10px" }}>
+                <div style={{ display: "flex", alignItems: "center", position: "relative" }}>
+                    <div style={{ height: "15px", width: "100%", position: "absolute", backgroundColor: "#808080ad", top: "5px" }}>
                         <p style={{ margin: 0, color: "white", fontSize: "10px", display: "flex", justifyContent: "center" }}>
                             {forecast ? forecast.name : ""}
                         </p>
                     </div>
                     <img src={props.forecast ? props.forecast.properties.periods[props.period].icon : ""} alt="" />
                 </div>
-                <div style={{ display: "flex", flexDirection: "column", height: "100%", marginLeft: "5px", justifyContent: "space-around" }}>
+                <div style={{ display: "flex", flexDirection: "column", height: "86px", marginLeft: "5px", justifyContent: "space-between" }}>
                     <div>
                         <p style={{ color: forecast.name.toLowerCase().includes("night") ? "#00b8ff" : "#e9220f", margin: 0 }}>
                             {forecast.name.toLowerCase().includes("night") ? "Low: " : "High: "}
@@ -36,8 +32,7 @@ export default function ForecastConditions(props: any) {
                 </div>
             </div>
         </div>
-    ) :
-        (
-            <p style={{ color: "white" }}>Something went wrong, try reloading the page</p>
-        )
+    ) : (
+        <p style={{ color: "white" }}>Something went wrong, try reloading the page</p>
+    )
 }
