d001061a35c2e888db891a297fd0e0dfd1c755be
Carter Dennis <carter.j.dennis@gmail.com>
2022-08-02 10:39:30 -0500

    Add route to get single user by id

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,6 +10,24 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id
 const ObjectId = require("mongodb").ObjectId;
 
+// Get a user by _id
+usersRoutes.route("/users/:id").get(function (req, response) {
+  let db_connect = dbo.getDb();
+  let query = { _id: ObjectId(req.params.id) };
+  db_connect
+    .collection("users")
+    .findOne(
+      query,
+      { projection: { password: 0 } },
+      function (err, user) {
+        if (err) {
+          throw (err);
+        } else {
+          response.json(user);
+        }
+      });
+});
+
 // Log in a user
 usersRoutes.route("/users/signin").post(function (req, response) {
   let db_connect = dbo.getDb();
