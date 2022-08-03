ac29dcfb22b0a97b00fe414932ee351f846899f6
Carter Dennis <carter.j.dennis@gmail.com>
2022-08-03 16:33:18 -0500

    Added ability to change email and password

--- client/src/components/account-box.tsx
+++ client/src/components/account-box.tsx
@@ -5,7 +5,7 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import * as React from "react";
 import isEmail from "validator/lib/isEmail";
-import { createUser, getUser, loginUser } from '../api/user';
+import { createUser, getUser, loginUser, updateUser } from '../api/user';
 
 const toggleButtonStyle = {
     "&.MuiToggleButton-root": {
@@ -67,10 +67,26 @@ const submitStyle = {
     }
 }
 
+const cancelStyle = {
+    "&.MuiButton-contained": {
+        color: "white",
+        backgroundColor: "#bb0909"
+    },
+    "&.MuiButton-contained:hover": {
+        backgroundColor: "red"
+    }
+}
+
 export default function AccountBox(props: any) {
     const [task, setTask] = React.useState<string>("log in");
     const [email, setEmail] = React.useState<{ email: string, isValid: boolean }>({ email: "", isValid: false });
     const [password, setPassword] = React.useState<{ password: string, isValid: boolean }>({ password: "", isValid: false });
+    const [changeEmail, setChangeEmail] = React.useState<boolean>(false);
+    const [newEmail, setNewEmail] = React.useState<{ email: string, isValid: boolean }>({ email: "", isValid: false });
+    const [confirmEmail, setConfirmEmail] = React.useState<string>("");
+    const [changePassword, setChangePassword] = React.useState<boolean>(false);
+    const [newPassword, setNewPassword] = React.useState<{ password: string, isValid: boolean }>({ password: "", isValid: false });
+    const [confirmPassword, setConfirmPassword] = React.useState<string>("");
 
     function validatePassword(value: string) {
         return (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(value));
@@ -84,12 +100,48 @@ export default function AccountBox(props: any) {
         }));
     }
 
+    const handleNewEmailChange = (event: any) => {
+        setNewEmail((prevState) => ({
+            ...prevState,
+            [event.target.name]: event.target.value,
+            isValid: isEmail(event.target.value)
+        }));
+    }
+
+    const handleConfirmEmailChange = (event: any) => {
+        setConfirmEmail(event.target.value);
+    }
+
     const handlePasswordChange = (event: any) => {
         setPassword((prevState) => ({
             ...prevState,
             [event.target.name]: event.target.value,
             isValid: validatePassword(event.target.value)
         }));
+    }
+
+    const handleNewPasswordChange = (event: any) => {
+        setNewPassword((prevState) => ({
+            ...prevState,
+            [event.target.name]: event.target.value,
+            isValid: validatePassword(event.target.value)
+        }));
+    }
+
+    const handleConfirmPasswordChange = (event: any) => {
+        setConfirmPassword(event.target.value);
+    }
+
+    const cancelChange = () => {
+        if (changeEmail) {
+            setNewEmail({ email: "", isValid: false });
+            setConfirmEmail("");
+            setChangeEmail(false);
+        } else {
+            setNewPassword({ password: "", isValid: false });
+            setConfirmPassword("");
+            setChangePassword(false);
+        }
     }
 
     const handleTaskChange = (event: React.MouseEvent<HTMLElement>, newTask: string) => {
@@ -147,6 +199,59 @@ export default function AccountBox(props: any) {
         props.setUser(null);
     }
 
+    const handleChangeEmail = () => {
+        setChangeEmail(!changeEmail);
+    }
+
+    const handleChangePassword = () => {
+        setChangePassword(!changePassword);
+    }
+
+    const submitEmailChange = async () => {
+        try {
+            updateUser({
+                _id: props.user._id,
+                user: {
+                    email: newEmail.email
+                }
+            }).then(async function (response) {
+                getUser({
+                    _id: props.user._id
+                }).then(async function (response) {
+                    props.setUser({
+                        _id: response._id,
+                        email: response.email,
+                        bookmarks: response.bookmarks
+                    });
+                })
+                setNewEmail({ email: "", isValid: false });
+                setConfirmEmail("");
+                setChangeEmail(false);
+            })
+        } catch (error) {
+            throw (error);
+        }
+    }
+
+    const submitPasswordChange = async () => {
+        try {
+            updateUser({
+                _id: props.user._id,
+                user: {
+                    password: newPassword.password
+                }
+            }).then(async function (response) {
+                console.log(response);
+                setNewPassword({ password: "", isValid: false });
+                setConfirmPassword("");
+                setChangePassword(false);
+
+            })
+        } catch (error) {
+            throw (error);
+        }
+    }
+
     return (
         <Box
             sx={{
@@ -161,15 +266,101 @@ export default function AccountBox(props: any) {
                 }}>
                     <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                         <p style={{ margin: "15px 0px 15px 0px" }}><b>Signed in as:</b></p>
-                        <p style={{ margin: "0px 0px 15px 0px" }}>{props.user.email}</p>
+                        <p style={{ margin: "0px" }}>{props.user.email}</p>
                     </div>
-                    <Button
-                        onClick={handleSignOut}
-                        variant="contained"
-                        sx={submitStyle}
-                    >
-                        Sign Out
-                    </Button>
+                    {!changeEmail && !changePassword ?
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <p
+                                style={{ textDecoration: "underline", cursor: "pointer", marginTop: "0px" }}
+                                onClick={handleChangeEmail}
+                            >
+                                Change email
+                            </p>
+                            <p
+                                style={{ textDecoration: "underline", cursor: "pointer" }}
+                                onClick={handleChangePassword}
+                            >
+                                Change password
+                            </p>
+                        </div> :
+                        changeEmail ?
+                            <div style={{ display: "flex", flexDirection: "column" }}>
+                                <TextField
+                                    value={newEmail.email}
+                                    onChange={handleNewEmailChange}
+                                    error={!newEmail.isValid && newEmail.email !== ""}
+                                    helperText={!newEmail.isValid && newEmail.email !== "" ? "Invalid email" : ""}
+                                    variant="standard"
+                                    label="New Email"
+                                    name="email"
+                                    sx={textFieldStyle}
+                                />
+                                <TextField
+                                    value={confirmEmail}
+                                    onChange={handleConfirmEmailChange}
+                                    variant="standard"
+                                    label="Confirm New Email"
+                                    disabled={!newEmail.isValid || newEmail.email === ""}
+                                    sx={textFieldStyle}
+                                />
+                                <Button
+                                    onClick={submitEmailChange}
+                                    variant={"contained"}
+                                    disabled={!newEmail.isValid || newEmail.email === "" || newEmail.email !== confirmEmail}
+                                    sx={submitStyle}
+                                >
+                                    Confirm
+                                </Button>
+                            </div> :
+                            <div style={{ display: "flex", flexDirection: "column" }}>
+                                <TextField
+                                    value={newPassword.password}
+                                    onChange={handleNewPasswordChange}
+                                    error={!newPassword.isValid && newPassword.password !== ""}
+                                    helperText={!newPassword.isValid && newPassword.password !== "" ? "Invalid password" : ""}
+                                    type="password"
+                                    variant="standard"
+                                    label="New Password"
+                                    name="password"
+                                    sx={textFieldStyle}
+                                />
+                                <TextField
+                                    value={confirmPassword}
+                                    onChange={handleConfirmPasswordChange}
+                                    type="password"
+                                    variant="standard"
+                                    label="Confirm New Password"
+                                    disabled={!newPassword.isValid || newPassword.password === ""}
+                                    sx={textFieldStyle}
+                                />
+                                <Button
+                                    onClick={submitPasswordChange}
+                                    variant={"contained"}
+                                    disabled={!newPassword.isValid || newPassword.password === "" || newPassword.password !== confirmPassword}
+                                    sx={submitStyle}
+                                >
+                                    Confirm
+                                </Button>
+                            </div>
+                    }
+                    {changeEmail || changePassword ?
+                        <div>
+                            <Button
+                                onClick={cancelChange}
+                                variant="contained"
+                                sx={cancelStyle}
+                            >
+                                Cancel
+                            </Button>
+                        </div> :
+                        <Button
+                            onClick={handleSignOut}
+                            variant="contained"
+                            sx={submitStyle}
+                        >
+                            Sign Out
+                        </Button>
+                    }
                 </div> :
                 <React.Fragment>
                     <ToggleButtonGroup
@@ -219,7 +410,7 @@ export default function AccountBox(props: any) {
                                 <Button
                                     onClick={handleSubmit}
                                     variant={"contained"}
-                                    disabled={!email.isValid || !password.password}
+                                    disabled={!email.isValid || !password.isValid}
                                     sx={submitStyle}
                                 >
                                     Submit
