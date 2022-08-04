e34109819eb1fd635ecc3089195a4376c47c232e
Carter Dennis <carter.j.dennis@gmail.com>
2022-08-04 15:06:03 -0500

    Preparing MERN app for Heroku deploy

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const app = express();
-const cors = require("cors")
-require("dotenv").config({ path: "./config.env" });
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config({path: "config.env"});
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
 // Routes
 app.use(require("./routes/users"));
 
 // Get driver connection
 const dbo = require("./db/conn");
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
     // Perform a database connection when server starts
