d283975cb9ca8b088447c4a9636bf3e5554f679a
Carter Dennis <carter.j.dennis@gmail.com>
2022-08-05 09:19:15 -0500

    Fixing dotenv config

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config({path: "config.env"});
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
@@ -24,7 +24,6 @@ app.listen(port, () => {
     // Perform a database connection when server starts
     dbo.connectToServer(function (err) {
         if (err) console.error(err);
-
     });
     console.log(`Server is running on port: ${port}`);
 });
