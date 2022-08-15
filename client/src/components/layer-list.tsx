6c0358fba50b2b4a9d2c55e89675ced6cd73ff98
Carter Dennis <carter.j.dennis@gmail.com>
2022-08-15 16:49:25 -0500

    Added visibility toggle and opacity slider to layer list

--- client/src/components/layer-list.tsx
+++ client/src/components/layer-list.tsx
@@ -1,13 +1,30 @@
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import TreeItem, { TreeItemProps } from '@mui/lab/TreeItem';
-import TreeView from '@mui/lab/TreeView';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import TreeItem, { TreeItemProps } from "@mui/lab/TreeItem";
+import TreeView from "@mui/lab/TreeView";
+import Box from "@mui/material/Box";
+import IconButton from '@mui/material/IconButton';
+import Slider from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+import Tooltip from '@mui/material/Tooltip';
+import Typography from "@mui/material/Typography";
+import * as React from "react";
 
 type StyledTreeItemProps = TreeItemProps & {
     layer: any,
     labelText: string
+}
+
+// Slider UI styling and label formatting
+const StyledSlider = styled(Slider)(({ theme }) => ({
+    "& .MuiSlider-valueLabel": {
+        backgroundColor: "#04283d"
+    }
+}));
+function valueLabelFormat(value: number) {
+    return ("Opacity: " + (Math.trunc((value * 100))) + "%");
 }
 
 function StyledTreeItem(props: StyledTreeItemProps) {
@@ -17,11 +34,25 @@ function StyledTreeItem(props: StyledTreeItemProps) {
         ...other
     } = props;
 
+    const [visible, setVisible] = React.useState<boolean>(layer.visible);
+    const [value, setValue] = React.useState<number | number[]>(layer.opacity);
+
+    const toggleVisibility = (event?: any) => {
+        event.stopPropagation();
+        setVisible(!layer.visible);
+        layer.visible = !layer.visible;
+    }
+
+    const handleSliderChange = (event: Event, newValue: number | number[]) => {
+        layer.opacity = newValue;
+        setValue(newValue);
+    }
+
     if (layer.type === "group" || (layer.sublayers && layer.sublayers.length > 0)) {
         return (
             <TreeItem
                 label={
-                    <Box style={{height: "40px"}}>
+                    <Box style={{ height: "40px" }}>
                         <Typography sx={{ color: "white" }}>
                             <b style={{ fontSize: "14px" }}>{labelText}</b>
                         </Typography>
@@ -34,11 +65,35 @@ function StyledTreeItem(props: StyledTreeItemProps) {
         return (
             <TreeItem
                 label={
-                    <Box>
-                        <Typography sx={{ color: "white" }}>
-                            <p style={{ fontSize: "12px" }}>{labelText}</p>
-                        </Typography>
-                    </Box>
+                    <React.Fragment>
+                        <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between", marginRight: "10px" }}>
+                            <Box>
+                                <Typography>
+                                    {labelText}
+                                </Typography>
+                            </Box>
+                            <Box sx={{ display: "flex", flexDirection: "row", width: "100%", alignItems: "center", justifyContent: "flex-end" }}>
+                                <Tooltip title="Toggle Visibility" enterDelay={2000} placement="left">
+                                    <IconButton onClick={(event) => toggleVisibility(event)} sx={{ color: "white" }}>
+                                        {visible ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                                    </IconButton>
+                                </Tooltip>
+                                <div style={{ width: "100px", display: "flex", flexDirection: "column", paddingRight: "20px" }}>
+                                    <StyledSlider
+                                        size="small"
+                                        step={0.01}
+                                        min={0}
+                                        max={1}
+                                        defaultValue={layer.opacity}
+                                        value={value}
+                                        onChange={handleSliderChange}
+                                        valueLabelDisplay="auto"
+                                        valueLabelFormat={valueLabelFormat}
+                                    />
+                                </div>
+                            </Box>
+                        </div>
+                    </React.Fragment>
                 }
                 {...other}
             />
